e9ae7c429b699dd11a114a50966bb1ea3579716b
Shalini-jha-dev <74014550+Shalini-jha-dev@users.noreply.github.com>
2021-07-01 17:35:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function takeSnapshot(){
 
 console.log = ("ml5 version:", ml5.version);
 
-classifier = ml5.imageClassifier("Mobilenet", modelLoaded)
+classifier = ml5.imageClassifier('Mobilenet', modelLoaded);
 
 function modelLoaded(){
     console.log("Model is Loaded !!");
